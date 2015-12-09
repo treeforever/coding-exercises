c529adda3f7b189d2ee0f9664e4cb2e879512285
Xiyang Chen <settinghead@gmail.com>
2015-12-09 02:08:43 -0500

    simplify insertion sort

--- sorting-verify/verify-sort.js
+++ sorting-verify/verify-sort.js
@@ -5,7 +5,7 @@ var jsc = require('jsverify'),
     insertionSort = require('./insertion-sort'),
     superSort = require('./super-awesome-sort.js');
 
-[bubbleSort, mergeSort, insertionSort, superSort].forEach(function(sortFn) {
+[bubbleSort, mergeSort, insertionSort].forEach(function(sortFn) {
   describe(sortFn.name, function () {
     jsc.property("correct", "array nat", function (arr) {
       return _.isEqual(sortFn(arr).slice(), arr.sort(function(a, b){return a-b}));
