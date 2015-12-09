c29fa29a2fc66182c960d9ef10b4a6089dd3951c
Mengqi Zhu <treeforever7@gmail.com>
2015-12-09 01:25:11 -0500

    fix verify-sort

---
+++ sorting-verify/super-awesome-sort.js
@@ -0,0 +1,5 @@
+function superSort(arr){
+  return arr;
+}
+
+module.exports = superSort;
--- sorting-verify/verify-sort.js
+++ sorting-verify/verify-sort.js
@@ -2,16 +2,17 @@ var jsc = require('jsverify'),
     _ = require('lodash'),
     bubbleSort = require('./bubble-sort'),
     mergeSort = require('./merge-sort'),
-    insertionSort = require('./insertion-sort');
+    insertionSort = require('./insertion-sort'),
+    superSort = require('./super-awesome-sort.js');
 
-[bubbleSort, mergeSort, insertionSort].forEach(function(sortFn) {
+[bubbleSort, mergeSort, insertionSort, superSort].forEach(function(sortFn) {
   describe(sortFn.name, function () {
     jsc.property("correct", "array nat", function (arr) {
-      return _.isEqual(sortFn(arr), arr.sort(function(a, b){return a-b}));
+      return _.isEqual(sortFn(arr).slice(), arr.sort(function(a, b){return a-b}));
     });
 
     jsc.property("idempotent", "array nat", function (arr) {
-      return _.isEqual(sortFn(bubbleSort(arr)), sortFn(arr));
+      return _.isEqual(sortFn(sortFn(arr)).slice(), sortFn(arr));
     });
   });
 });
