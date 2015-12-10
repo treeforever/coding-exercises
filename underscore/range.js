ce7c76feb3bd7ccbec3f6ac9e02adb61af3504da
Mengqi Zhu <treeforever7@gmail.com>
2015-12-09 23:40:09 -0500

    add ultimateRange
    using "arguments"

--- underscore/range.js
+++ underscore/range.js
@@ -13,3 +13,19 @@ function rangeStartStop(start, stop){
 function rangeStop(stop){
   return rangeStartStopStep(0, stop, 1);
 }
+
+function ultimateRange(){
+  if(arguments.length === 3){
+    return rangeStartStopStep(arguments[0], arguments[1], arguments[2]);
+  } else if(arguments.length === 2){
+    return rangeStartStop(arguments[0], arguments[1]);
+  } else{
+    return rangeStop(arguments[0]);
+  }
+}
+
+console.log(ultimateRange(4, -2, -1));
+
+module.exports = rangeStartStopStep;
+module.exports = rangeStartStop;
+module.exports = rangeStop;
