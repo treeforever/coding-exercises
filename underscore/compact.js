86f134176955739aff751069c206cdd27ec97893
Mengqi Zhu <treeforever7@gmail.com>
2015-12-15 11:39:24 -0500

    init: compact

---
+++ underscore/compact.js
@@ -0,0 +1,27 @@
+function isFalsy(a){
+  if(a === false || a === undefined || a === 0 || a === null || ((typeof a !== "string") && isNaN(a)) || a === ""
+){
+  return a;
+  }
+}
+
+function reduceOneElement(array, falsyValue){
+  var newArr = [];
+  for(j=0; j<array.length;j++){
+    if(array[j] !== falsyValue){
+        newArr.push(array[j]);
+    }
+  }
+  return newArr;
+}
+
+function compact(array){
+  var updatedArr = array;
+  for(i=0;i<array.length;i++){
+    falsyValue = isFalsy(array[i]);
+    updatedArr = reduceOneElement(updatedArr,falsyValue);
+  }
+  return updatedArr;
+}
+
+// console.log(compact([0, 1, false, 2, '', null, "chi", 3]));
