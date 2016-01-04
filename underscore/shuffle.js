0a822af9409df0b513072894075d238f28efd40f
Mengqi Zhu <treeforever7@gmail.com>
2016-01-04 01:28:58 -0500

    shuffle2
    used Pencil-and-paper method, but the function is possibly inefficient
    when it comes to a long list of number.

--- underscore/shuffle.js
+++ underscore/shuffle.js
@@ -1,5 +1,5 @@
 var testList = [12,34,51,4,7,-1,809,1111,22,45,6666,77,88,123,653,254,798];
-
+testList2 = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 function shuffle(list){
   if (arguments.length>0){
@@ -19,4 +19,25 @@ function shuffle(list){
   }
 }
 
-console.log(shuffle(testList));
+function shuffle2(list){
+  if (arguments.length>0){
+    var multiplier = list.length;
+        listCopy = list;
+        shuffledList = [];
+
+    for (i=0;i<list.length;i++){
+      randomPosition = Math.floor(Math.random() * multiplier);
+
+      while(listCopy[randomPosition] === undefined){
+        randomPosition = Math.floor(Math.random() * multiplier);
+      }
+      shuffledList[i] = listCopy[randomPosition];
+      delete listCopy[randomPosition];
+    }
+    return shuffledList;
+  } else {
+    return [];
+  }
+}
+
+console.log(shuffle2(testList2));
